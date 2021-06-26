f320697a40bd7c7c3164b364bee6fabdb0370ad7
CIRCULARKA <donkardon@list.ru>
2021-06-26 16:40:59 +0500

    Prepare model for serialization

--- src/Models/CryptoCurrency.cs
+++ src/Models/CryptoCurrency.cs
@@ -1,13 +1,24 @@
-using System;
+using Newtonsoft.Json;
 
 namespace CryptocurrenciesViewer.Models
 {
 	public class CryptoCurrency : IModel
 	{
+		public int ID { get; set; }
+
 		public string Name { get; set; }
 
 		public string Symbol { get; set; }
 
+		public decimal Price { get; set; }
+
+		[JsonProperty(PropertyName = "market_cap")]
 		public decimal MarketCapitalization { get; set; }
+
+		[JsonProperty(PropertyName = "percent_change_24h")]
+		public decimal Last24HoursDynamics { get; set; }
+
+		[JsonProperty(PropertyName = "percent_change_1h")]
+		public decimal Last1HourDynamics { get; set; }
 	}
 }
