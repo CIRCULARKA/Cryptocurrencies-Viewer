98c15371898fc8fa811c96ad7cd921b5dffde202
CIRCULARKA <donkardon@list.ru>
2021-06-26 13:39:11 +0500

    Init project

---
+++ Startup.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CryptocurrenciesViewer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) { }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) { }
+    }
+}
