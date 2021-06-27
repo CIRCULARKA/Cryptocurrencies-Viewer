e9e531990e294178506016ed4d166d172e0bc522
CIRCULARKA <donkardon@list.ru>
2021-06-27 17:22:48 +0500

    Implement deserializer

--- src/CoinMarketApi/CoinMarketApiDeserializer.cs
+++ src/CoinMarketApi/CoinMarketApiDeserializer.cs
@@ -1,13 +1,32 @@
 using System.Collections.Generic;
 using CryptocurrenciesViewer.Models;
+using Newtonsoft.Json.Linq;
 
 namespace CryptocurrenciesViewer.CoinMarketApi
 {
 	public class CoinMarketApiDeserializer
 	{
-		IEnumerable<CryptoCurrency> DeserializeJsonData(string data)
+		public IEnumerable<CryptoCurrency> DeserializeJsonData(string data)
 		{
-			return null;
+			var result = new List<CryptoCurrency>();
+
+			foreach (var node in JObject.Parse(data)["data"])
+			{
+				var usdNode = node["quote"]["USD"];
+
+				var currency = new CryptoCurrency() {
+					ID = (int)node["id"],
+					Name = (string)node["name"],
+					MarketCapitalization = (decimal)usdNode["market_cup"],
+					Price = (decimal)usdNode["price"],
+					Last1HourDynamics = (decimal)usdNode["percent_change_1h"],
+					Last24HoursDynamics = (decimal)usdNode["percent_change_24h"]
+				};
+
+				result.Add(currency);
+			}
+
+			return result;
 		}
 	}
 }
