4b0aff52de9357f1dd4fc4de88cd7b7db7e7506c
CIRCULARKA <donkardon@list.ru>
2021-06-27 21:11:56 +0500

    Add second test for crypto currency provider

--- Tests/CoinMarketApi/CryptoCurrencyProviderTests.cs
+++ Tests/CoinMarketApi/CryptoCurrencyProviderTests.cs
@@ -1,7 +1,9 @@
 using Moq;
 using Xunit;
+using System.Linq;
 using System.Collections.Generic;
 using CryptocurrenciesViewer.CoinMarketApi;
+using CryptocurrenciesViewer.Models;
 
 namespace CryptocurrenciesViewer.Tests
 {
@@ -29,6 +31,35 @@ namespace CryptocurrenciesViewer.Tests
 
 			// Assert
 			Assert.Equal("http://endpoint/?limit=2300&start=53&sort=cmc_rank", provider.Request);
+		}
+
+		[Fact]
+		public void IsReturnedCurrencyCorrect()
+		{
+			// Arrange
+			var apiProviderMock = new Mock<IApiProvider>();
+			apiProviderMock.
+				Setup(p => p.GetApiKey()).
+					// This is sandbox api key from https://pro.coinmarketcap.com/api/v1#section/Quick-Start-Guide
+					Returns("b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c");
+
+			var deserializer = new CryptoCurrencyDeserializer();
+
+			var provider = new CryptoCurrencyProvider(
+				"sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+				new Dictionary<string, string> {
+					{ "limit", "1" }
+				},
+				deserializer,
+				apiProviderMock.Object
+			);
+
+			// Act
+			IEnumerable<CryptoCurrency> result =
+				provider.GetCurrencyFromRemoteServer();
+
+			// Assert
+			Assert.True(result.Count() > 0);
 		}
 	}
 }
