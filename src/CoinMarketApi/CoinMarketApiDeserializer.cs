0cbb11218c3a92c440370ba7b52986f46371c6ea
CIRCULARKA <donkardon@list.ru>
2021-06-27 17:30:11 +0500

    Add simple exception handling and Symbol property deserialization

--- src/CoinMarketApi/CoinMarketApiDeserializer.cs
+++ src/CoinMarketApi/CoinMarketApiDeserializer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using CryptocurrenciesViewer.Models;
 using Newtonsoft.Json.Linq;
@@ -10,20 +11,32 @@ namespace CryptocurrenciesViewer.CoinMarketApi
 		{
 			var result = new List<CryptoCurrency>();
 
-			foreach (var node in JObject.Parse(data)["data"])
+			try
 			{
-				var usdNode = node["quote"]["USD"];
+				foreach (var node in JObject.Parse(data)["data"])
+				{
+					var usdNode = node["quote"]["USD"];
 
-				var currency = new CryptoCurrency() {
-					ID = (int)node["id"],
-					Name = (string)node["name"],
-					MarketCapitalization = (decimal)usdNode["market_cup"],
-					Price = (decimal)usdNode["price"],
-					Last1HourDynamics = (decimal)usdNode["percent_change_1h"],
-					Last24HoursDynamics = (decimal)usdNode["percent_change_24h"]
-				};
+					var currency = new CryptoCurrency() {
+						ID = (int)node["id"],
+						Name = (string)node["name"],
+						Symbol = (string)node["symbol"],
+						MarketCapitalization = (decimal)usdNode["market_cup"],
+						Price = (decimal)usdNode["price"],
+						Last1HourDynamics = (decimal)usdNode["percent_change_1h"],
+						Last24HoursDynamics = (decimal)usdNode["percent_change_24h"]
+					};
 
-				result.Add(currency);
+					result.Add(currency);
+				}
+			}
+			catch
+			{
+				throw new Exception(
+					"Incorrect data format. " +
+					"Learn which data format must be provided for this deserializer here " +
+					"https://pro.coinmarketcap.com/api/v1#operation/getV1CryptocurrencyListingsLatest"
+				);
 			}
 
 			return result;
