16fba3a6eef781967076e62c8f9843d851e922dd
CIRCULARKA <donkardon@list.ru>
2021-06-27 22:56:48 +0500

    Add repo to controller and pull data from it

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using CryptocurrenciesViewer.Models;
+using CryptocurrenciesViewer.Models.Factories;
 
 namespace CryptocurrenciesViewer
 {
 	public class HomeController : Controller
 	{
+		private IRepository _repository;
+
+		public HomeController()
+		{
+			_repository = new RepositoryFactory().
+				CreateDefaultRepository();
+		}
+
 		public IActionResult GetCryptocurrenciesList()
 		{
-			return View("Cryptocurrencies");
+			_repository.RefreshData();
+			_repository.SaveChanges();
+
+			return View(
+				viewName: "Cryptocurrencies",
+				model: _repository.AllCurrency
+			);
 		}
 	}
 }
