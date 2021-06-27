cffa4575e484b2c29367cf996daae392980697cc
CIRCULARKA <donkardon@list.ru>
2021-06-27 22:55:28 +0500

    Ensure DB is created on DbContext creating

--- src/Models/DefaultDbContext.cs
+++ src/Models/DefaultDbContext.cs
@@ -4,6 +4,11 @@ namespace CryptocurrenciesViewer.Models
 {
 	public class DefaultDbContext : DbContextBase
 	{
+		public DefaultDbContext()
+		{
+			Database.EnsureCreated();
+		}
+
 		protected override void OnConfiguring(DbContextOptionsBuilder builder) =>
 			builder.UseSqlServer("Server=(local);Database=CryptoCurrenciesDB;Trusted_Connection=True");
 
