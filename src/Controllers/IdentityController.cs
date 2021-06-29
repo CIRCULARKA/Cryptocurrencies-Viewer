28337cd5092ce35b695ea258d67a928314d73baf
CIRCULARKA <donkardon@list.ru>
2021-06-29 15:49:49 +0500

    Add registration to IdentityController

--- src/Controllers/IdentityController.cs
+++ src/Controllers/IdentityController.cs
@@ -1,11 +1,50 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using CurrencyViewer.Models;
+using CurrencyViewer.ViewModels;
 
 namespace CurrencyViewer.Controllers
 {
 	public class IdentityController : Controller
 	{
 		private UserManager<User> _usersManager;
+
+		private SignInManager<User> _signInManager;
+
+		public IdentityController(UserManager<User> usersManager,
+			SignInManager<User> signInManager
+		)
+		{
+			_usersManager = usersManager;
+			_signInManager = signInManager;
+		}
+
+		public IActionResult GetRegistrationPage() =>
+			View("Registration");
+
+		[HttpPost]
+		public async Task<IActionResult> RegisterUser(RegistrationViewModel model)
+		{
+			if (!ModelState.IsValid) return View("Registration", model);
+
+			var newUser = new User {
+				Email = model.Email
+			};
+
+			var registrationResult = await _usersManager.
+				CreateAsync(newUser, model.Password);
+
+			if (!registrationResult.Succeeded)
+				foreach (var error in registrationResult.Errors)
+					ModelState.AddModelError(string.Empty, error.Description);
+
+			await _signInManager.SignInAsync(newUser, isPersistent: true);
+
+			return RedirectToAction(
+				actionName: "GetCryptocurrenciesList",
+				controllerName: "Home"
+			);
+		}
 	}
 }
