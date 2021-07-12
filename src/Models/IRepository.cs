28e893e544ffe070c7e89995f8ab485eaf731c5c
CIRCULARKA <donkaron@list.ru>
2021-07-12 14:26:41 +0500

    Add new public method to repository interface and provide documentation

--- src/Models/IRepository.cs
+++ src/Models/IRepository.cs
@@ -6,7 +6,19 @@ namespace CurrencyViewer.Models
 	{
 		IEnumerable<CryptoCurrency> AllCurrency { get; }
 
+		/// <summary>
+		/// Refreshes data via API in database
+		///	Takes no effect if DB is empty.
+		/// </summary>
 		void RefreshCurrencyInfo();
+
+		/// <summary>
+		/// Populates DB with data from server via API
+		/// if DB is empty.
+		/// Takes no effect if DB already has data.
+		/// Use RefreshCurrencyInfo method if DB already has data.
+		/// </summary>
+		void LoadCurrencyFromServer();
 
 		void SaveChanges();
 	}
