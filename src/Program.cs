1310b95d8d14f81d5ebe555cb5ae3b0686ce4e79
CIRCULARKA <donkaron@list.ru>
2021-08-02 18:04:31 +0500

    Give user a possibility to inspect runtime errors that crashes whole application

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -7,7 +8,14 @@ namespace CurrencyViewer
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch
+            {
+                Console.ReadLine();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
